4401940ddef29e902544115e2a21527f9d8a7082
Mahdi Zarrintareh <mza2rintareh@gmail.com>
2020-02-10 23:42:55 +0330

    Fix db session close bug

--- src/anilius/core/model.py
+++ src/anilius/core/model.py
@@ -1,5 +1,6 @@
 import uuid
 
+from anilius.db.db_session import DBSession
 from sqlalchemy import Column, DateTime
 from sqlalchemy.sql import func
 
@@ -16,12 +17,10 @@ class BaseModel(Model):
     updated_on = Column(DateTime, default=func.now(), onupdate=func.now())
 
     @classmethod
-    def create(cls, commit=True, **entity_properties):
+    def create(cls, commit=True, close=True, **entity_properties):
         entity = cls(**entity_properties)
-        db_session().add(entity)
-
-        if commit:
-            db_session.commit()
+        with DBSession(commit=commit, close=close) as session:
+            session.add(entity)
 
         return entity
 
@@ -29,11 +28,12 @@ class BaseModel(Model):
     def get(cls, entity_id):
         if not validate_uuid4(entity_id):
             raise ValueError("entity id should be uuid4")
-
-        entity = db_session().query(cls).get(entity_id)
+        with DBSession(commit=False) as session:
+            entity = session.query(cls).get(entity_id)
         return entity
 
     @classmethod
     def list(cls, condition_dict):
-        entity = db_session().query(cls).filter(*condition_dict).all()
+        with DBSession(commit=False) as session:
+            entity = session.query(cls).filter(*condition_dict).all()
         return entity
