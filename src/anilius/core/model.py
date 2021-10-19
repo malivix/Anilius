6be32ad70c46ec069911c10486e5ca8fe90b6d7b
Mahdi Zarrintareh <mza2rintareh@gmail.com>
2021-10-19 13:44:40 +0330

    fix bug for delete query

--- src/anilius/core/model.py
+++ src/anilius/core/model.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import Dict
 
 from anilius.db.db import Model
 from anilius.db.db_session import DBSession
@@ -40,20 +41,11 @@ class BaseModel(Model):
         return entity
 
     @classmethod
-    def delete(cls, entity_id: "UUID") -> None:
-        """deletes the given entry from database table
+    def delete(cls, **entity_properties) -> None:
+        """deletes the given entry from database table"""
 
-        Args:
-            entity_id (UUID): the id of the entry to be deleted
-
-        Raises:
-            ValueError: raised when the given id is not uuid4
-        """
-
-        if not validate_uuid4(entity_id):
-            raise ValueError("entity id should be uuid4")
         with DBSession(commit=False) as session:
-            entity = session.query(cls).get(entity_id)
+            entity = session.query(cls).filter_by(**entity_properties).first()
             session.delete(entity)
         return
 
@@ -75,4 +67,3 @@ class BaseModel(Model):
             entity = session.query(cls).get(entity_id)
             entity.update(new_fields)
         return
-
